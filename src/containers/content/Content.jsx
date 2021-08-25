2c95ab9aadd05165a5ac7c374a143e7150ddd66c
DenzelHawking <ankhid98@gmail.com>
2021-08-25 12:25:48 +0500

    routes added

--- src/containers/content/Content.jsx
+++ src/containers/content/Content.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Main from '../../tabs/Main';
+import Home from '../../tabs/Home';
+import Packages from '../../tabs/Packages';
+import News from '../../tabs/News';
+import Contacts from '../../tabs/Contacts';
 import './content.css';
 
 function Content() {
   return (
     <main>
       <Switch>
-        <Route path='/' exact component={Main} />
+        <Route path='/' exact component={Home} />
+        <Route path='/packages' exact component={Packages} />
+        <Route path='/news' exact component={News} />
+        <Route path='/contacts' exact component={Contacts} />
       </Switch>
     </main>
   );
