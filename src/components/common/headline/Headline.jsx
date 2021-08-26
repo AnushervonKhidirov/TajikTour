7c7497a295abae34c966816dafb6b08411454757
DenzelHawking <ankhid98@gmail.com>
2021-08-26 08:26:33 +0500

    light mode added to headline

--- src/components/common/headline/Headline.jsx
+++ src/components/common/headline/Headline.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './headline.css';
 
 export default function Headline(props) {
   const [color, setColor] = useState('#555');
+
+  useEffect(() => {
+    if (props.light) {
+      setColor('#fff')
+    }
+  }, [props.light]);
 
   return <h1 className="headline" style={{color: color}}><span>{props.title}</span></h1>
 }
