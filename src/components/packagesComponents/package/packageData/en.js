452b6fd7be0b2f8237d8e2ea13b3c3544e93a4b1
DenzelHawking <ankhid98@gmail.com>
2021-09-05 17:25:26 +0500

    package page done

--- src/components/packagesComponents/package/packageData/en.js
+++ src/components/packagesComponents/package/packageData/en.js
@@ -24,18 +24,135 @@ export const packages = {
       'Lorem ipsum dolor sit amet, consectetur adipiscing'
     ],
     sendForm: 'Send From'
+  },
+  bora_bora: {
+    tourName: 'Bora Bora',
+    img: 'bora_bora.jpg',
+    experience: 'Amazing Experience',
+    experienceDesc: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
+    ],
+    gallery: 'Gallery',
+    galleryImages: [
+        'koh_samui-0.jpg',
+        'koh_samui-1.jpg',
+        'koh_samui-2.jpg',
+        'koh_samui-3.jpg'
+    ],
+    dayByDay: 'Day by Day',
+    dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
+    dayByDayList: [
+      'Departure at the airport and arrival at the hotel',
+      'Visit the main museums and lunch included',
+      'Excursion in the natural oasis and picnic',
+      'Transfer to the airport and return to the agency',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    ],
+    sendForm: 'Send From'
+  },
+  maldives: {
+    tourName: 'Maldives',
+    img: 'maldives.jpg',
+    experience: 'Amazing Experience',
+    experienceDesc: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
+    ],
+    gallery: 'Gallery',
+    galleryImages: [
+        'koh_samui-0.jpg',
+        'koh_samui-1.jpg',
+        'koh_samui-2.jpg',
+        'koh_samui-3.jpg'
+    ],
+    dayByDay: 'Day by Day',
+    dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
+    dayByDayList: [
+      'Departure at the airport and arrival at the hotel',
+      'Visit the main museums and lunch included',
+      'Excursion in the natural oasis and picnic',
+      'Transfer to the airport and return to the agency',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    ],
+    sendForm: 'Send From'
+  },
+  phuket: {
+    tourName: 'Phuket',
+    img: 'phuket.jpg',
+    experience: 'Amazing Experience',
+    experienceDesc: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
+    ],
+    gallery: 'Gallery',
+    galleryImages: [
+        'koh_samui-0.jpg',
+        'koh_samui-1.jpg',
+        'koh_samui-2.jpg',
+        'koh_samui-3.jpg'
+    ],
+    dayByDay: 'Day by Day',
+    dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
+    dayByDayList: [
+      'Departure at the airport and arrival at the hotel',
+      'Visit the main museums and lunch included',
+      'Excursion in the natural oasis and picnic',
+      'Transfer to the airport and return to the agency',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    ],
+    sendForm: 'Send From'
+  },
+  seychelles: {
+    tourName: 'Seychelles',
+    img: 'seychelles.jpg',
+    experience: 'Amazing Experience',
+    experienceDesc: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
+    ],
+    gallery: 'Gallery',
+    galleryImages: [
+        'koh_samui-0.jpg',
+        'koh_samui-1.jpg',
+        'koh_samui-2.jpg',
+        'koh_samui-3.jpg'
+    ],
+    dayByDay: 'Day by Day',
+    dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
+    dayByDayList: [
+      'Departure at the airport and arrival at the hotel',
+      'Visit the main museums and lunch included',
+      'Excursion in the natural oasis and picnic',
+      'Transfer to the airport and return to the agency',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    ],
+    sendForm: 'Send From'
+  },
+  hawaii: {
+    tourName: 'Hawaii',
+    img: 'hawaii.jpg',
+    experience: 'Amazing Experience',
+    experienceDesc: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
+    ],
+    gallery: 'Gallery',
+    galleryImages: [
+        'koh_samui-0.jpg',
+        'koh_samui-1.jpg',
+        'koh_samui-2.jpg',
+        'koh_samui-3.jpg'
+    ],
+    dayByDay: 'Day by Day',
+    dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
+    dayByDayList: [
+      'Departure at the airport and arrival at the hotel',
+      'Visit the main museums and lunch included',
+      'Excursion in the natural oasis and picnic',
+      'Transfer to the airport and return to the agency',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    ],
+    sendForm: 'Send From'
   }
 }
-
-// koh_samui.jpg
-// bora_bora.jpg
-// maldives.jpg
-// phuket.jpg
-// seychelles.jpg
-// hawaii.jpg
-// kohSamui: 'Koh Samui',
-// boraBora: 'Bora Bora',
-// maldives: 'Maldives',
-// hawaii: 'Hawaii',
-// seychelles: 'Seychelles',
-// phuket: 'Phuket',
--- src/components/packagesComponents/package/packageData/ru.js
+++ src/components/packagesComponents/package/packageData/ru.js
@@ -2,19 +2,149 @@ export const packages = {
   koh_samui: {
     tourName: 'Самуй',
     img: 'koh_samui.jpg',
-    experience: 'Amazing Experience',
+    experience: 'Удивительный опыт',
     experienceDesc: [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
     ],
-    gallery: 'Gallery',
+    gallery: 'Галерея',
     galleryImages: [
         'koh_samui-0.jpg',
         'koh_samui-1.jpg',
         'koh_samui-2.jpg',
         'koh_samui-3.jpg'
     ],
-    dayByDay: 'Day by Day',
+    dayByDay: 'Дни',
+    dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
+    dayByDayList: [
+      'Departure at the airport and arrival at the hotel',
+      'Visit the main museums and lunch included',
+      'Excursion in the natural oasis and picnic',
+      'Transfer to the airport and return to the agency',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    ],
+    sendForm: 'Отправить форму'
+  },
+  bora_bora: {
+    tourName: 'Бора Бора',
+    img: 'bora_bora.jpg',
+    experience: 'Удивительный опыт',
+    experienceDesc: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
+    ],
+    gallery: 'Галерея',
+    galleryImages: [
+        'koh_samui-0.jpg',
+        'koh_samui-1.jpg',
+        'koh_samui-2.jpg',
+        'koh_samui-3.jpg'
+    ],
+    dayByDay: 'Дни',
+    dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
+    dayByDayList: [
+      'Departure at the airport and arrival at the hotel',
+      'Visit the main museums and lunch included',
+      'Excursion in the natural oasis and picnic',
+      'Transfer to the airport and return to the agency',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    ],
+    sendForm: 'Отправить форму'
+  },
+  maldives: {
+    tourName: 'Мальдивы',
+    img: 'maldives.jpg',
+    experience: 'Удивительный опыт',
+    experienceDesc: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
+    ],
+    gallery: 'Галерея',
+    galleryImages: [
+        'koh_samui-0.jpg',
+        'koh_samui-1.jpg',
+        'koh_samui-2.jpg',
+        'koh_samui-3.jpg'
+    ],
+    dayByDay: 'Дни',
+    dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
+    dayByDayList: [
+      'Departure at the airport and arrival at the hotel',
+      'Visit the main museums and lunch included',
+      'Excursion in the natural oasis and picnic',
+      'Transfer to the airport and return to the agency',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    ],
+    sendForm: 'Отправить форму'
+  },
+  phuket: {
+    tourName: 'Пхукет',
+    img: 'phuket.jpg',
+    experience: 'Удивительный опыт',
+    experienceDesc: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
+    ],
+    gallery: 'Галерея',
+    galleryImages: [
+        'koh_samui-0.jpg',
+        'koh_samui-1.jpg',
+        'koh_samui-2.jpg',
+        'koh_samui-3.jpg'
+    ],
+    dayByDay: 'Дни',
+    dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
+    dayByDayList: [
+      'Departure at the airport and arrival at the hotel',
+      'Visit the main museums and lunch included',
+      'Excursion in the natural oasis and picnic',
+      'Transfer to the airport and return to the agency',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    ],
+    sendForm: 'Отправить форму'
+  },
+  seychelles: {
+    tourName: 'Сейшелы',
+    img: 'seychelles.jpg',
+    experience: 'Удивительный опыт',
+    experienceDesc: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
+    ],
+    gallery: 'Галерея',
+    galleryImages: [
+        'koh_samui-0.jpg',
+        'koh_samui-1.jpg',
+        'koh_samui-2.jpg',
+        'koh_samui-3.jpg'
+    ],
+    dayByDay: 'Дни',
+    dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
+    dayByDayList: [
+      'Departure at the airport and arrival at the hotel',
+      'Visit the main museums and lunch included',
+      'Excursion in the natural oasis and picnic',
+      'Transfer to the airport and return to the agency',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    ],
+    sendForm: 'Отправить форму'
+  },
+  hawaii: {
+    tourName: 'Гаваи',
+    img: 'hawaii.jpg',
+    experience: 'Удивительный опыт',
+    experienceDesc: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus.'
+    ],
+    gallery: 'Галерея',
+    galleryImages: [
+        'koh_samui-0.jpg',
+        'koh_samui-1.jpg',
+        'koh_samui-2.jpg',
+        'koh_samui-3.jpg'
+    ],
+    dayByDay: 'Дни',
     dayByDayDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut venenatis lorem. Nunc sodales interdum rhoncus. Nulla a leo finibus, ullamcorper lorem vel, scelerisque massa. Vivamus leo dui, interdum non fermentum eget, laoreet ac lorem. Aliquam a ultricies nisl. Nulla consequat lobortis urna sed cursus. ipsum dolor sit amet, consectetur adipiscing elit consectef lorem vel, scelerisque massa. Vivamus leo dui, interdum non.',
     dayByDayList: [
       'Departure at the airport and arrival at the hotel',
