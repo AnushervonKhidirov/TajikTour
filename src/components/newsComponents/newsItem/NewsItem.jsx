fea3125e6e226a0b6aaa604c0ed6d511983fecb5
DenzelHawking <ankhid98@gmail.com>
2021-09-10 08:10:49 +0500

    news added

---
+++ src/components/newsComponents/newsItem/NewsItem.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useEffect, useContext} from 'react';
+import { WrapperContext } from '../../../Context';
+import styles from './NewsItem.module.css';
+
+function News({ newsItem }) {
+  const wrapper = useContext(WrapperContext);
+  const [news, setNews] = useState({});
+
+  // add error page !!!
+  useEffect(() => import(`./newsData/${wrapper.lang}`).then(news => setNews(news.newsData[newsItem] || {})), [wrapper.lang, newsItem]);
+
+  return (
+    <div className={`${styles.news_item_wrapper} block_item`}>
+      <h2 className={styles.headline}>{news.title}</h2>
+      <div className={styles.image} style={{ backgroundImage: `url(/img/news/${news.image})` }}></div>
+
+      <div className={styles.desc}>
+        {news.desc && news.desc.map((desc, index) => {
+          return <NewsParagraph text={desc} key={`news_paragraph-${index}`} />
+        })}
+      </div>
+    </div>
+  );
+}
+
+function NewsParagraph({ text }) {
+  return (
+    <p className={styles.paragraph}>{text}</p>
+  );
+}
+
+export default News;
---
+++ src/components/newsComponents/newsList/NewsList.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
+import { WrapperContext } from '../../../Context';
+import { Link } from 'react-router-dom';
+import Headline from '../../common/headline/Headline';
+import styles from './NewsList.module.css'
+
+function NewsList() {
+  const wrapper = useContext(WrapperContext);
+  const path = useLocation().pathname;
+  const [newsList, setNewsList] = useState([]);
+  const [locales, setLocales] = useState({});
+
+  useEffect(() => import(`./newsListData/${wrapper.lang}`).then(newsList => {
+    setNewsList(newsList.newsListData);
+    setLocales(newsList.locales);
+  }), [wrapper.lang]);
+
+  return (
+    <div className={styles.news_list_wrapper}>
+      <Headline title={locales.title} />
+      <div className={styles.news_list}>
+        {newsList.map(news => {
+          return <NewsItem news={news} path={path} key={news.newsKey} />
+        })}
+      </div>
+    </div>
+  );
+}
+
+function NewsItem({ news, path }) {
+  return (
+    <div className={styles.news}>
+      <div className={styles.news_img} style={{ backgroundImage: `url(/img/news/${news.newsImg})` }} ></div>
+      <div className={styles.text}>
+        <div className={styles.type}>{news.newsType}</div>
+        <h3 className={styles.title}>{news.newsTitle}</h3>
+        <Link to={`${path}/${news.newsKey}`}>{news.linkText}</Link>
+      </div>
+    </div>
+  );
+}
+
+export default NewsList;
