21f0a43add0c018eb89c18e9a5989f1e557ff3f3
DenzelHawking <ankhid98@gmail.com>
2021-09-10 08:12:41 +0500

    news added

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import NewsList from '../components/newsComponents/newsList/NewsList';
+import NewsItem from '../components/newsComponents/newsItem/NewsItem';
 
 function News() {
-  return (
-    <div>news</div>
-  );
+  const { newsItem } = useParams()
+  return newsItem ? <NewsItem newsItem={newsItem} /> : <NewsList />;
 }
 
 export default News;
